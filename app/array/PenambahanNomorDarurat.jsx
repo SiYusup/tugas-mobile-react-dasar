9cab9f8d87adc41b9b66cd1a52075606f99a66df
UCrazy <finder.ucup@gmail.com>
2026-09-07 15:00:29 +0700

    UCUP : Telah mengerjakan soal

---
+++ app/array/PenambahanNomorDarurat.jsx
@@ -0,0 +1,9 @@
+//
+
+function PenambahanNomorDarurat() {
+  let darurat = ["Kebakaran", "Kecelakaan"];
+
+  darurat.unshift("Madis Darurat");
+  darurat.shift();
+}
+export default PenambahanNomorDarurat;
